972f330c9be5306c6eec9c75599f6a96bc268eec
kabaniery <mrscorpio93@gmail.com>
2025-05-30 18:47:23 +0300

    Эффекты для биома

--- src/main/java/com/kabaniery/uncommonadventure/world/biome_source/GeneralBiomes.java
+++ src/main/java/com/kabaniery/uncommonadventure/world/biome_source/GeneralBiomes.java
@@ -10,10 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeGenerationSettings;
-import net.minecraft.world.level.biome.BiomeSource;
-import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.biome.*;
 import net.minecraftforge.common.world.MobSpawnSettingsBuilder;
 
 public class GeneralBiomes {
@@ -36,12 +33,19 @@ public class GeneralBiomes {
         BiomeGenerationSettings.Builder biomeBuilder =
                 new BiomeGenerationSettings.Builder(context.lookup(Registries.PLACED_FEATURE), context.lookup(Registries.CONFIGURED_CARVER));
 
+        BiomeSpecialEffects specialEffects = new BiomeSpecialEffects.Builder()
+                .fogColor(0xC0D8FF)              // example: light blue fog
+                .waterColor(0x3F76E4)            // example: standard water color
+                .waterFogColor(0x050533)         // example: dark blue water fog
+                .skyColor(0x77ADFF)              // example: sky color
+                .build();
 
         return new Biome.BiomeBuilder()
                 .downfall(0.8f)
                 .temperature(0.0f)
                 .generationSettings(biomeBuilder.build())
                 .mobSpawnSettings(spawnBuilder.build())
+                .specialEffects(specialEffects)
                 .build();
     }
 }
